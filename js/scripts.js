86470c1eb5717d65a14652792515c78d6f92b119
Leandro Dias <45826681+leandroxxx741@users.noreply.github.com>
2021-12-09 21:57:11 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
+
 function openNav() {
 
-    var x = document.getElementById("navigation");
-    
-    if (x.className === "navigation") {
-        x.className += " menujs";
-        document.getElementById("threeline-icon").innerHTML = "&Cross;";
-    
-    }  else {
-        x.className = "navigation";
-        document.getElementById("threeline-icon").innerHTML = "&#9776;";
+	var x =  document.getElementById("navigation");
 
-    }
+	if (x.className === "navigation") {
+		x.className += " menujs";
+		document.getElementById("threeline-icon").innerHTML = "&Cross;";
+
+	} else {
+		x.className = "navigation";
+		document.getElementById("threeline-icon").innerHTML = "&#9776;";
+
+	}
+
+
 
 }
